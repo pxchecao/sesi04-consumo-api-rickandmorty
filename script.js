abd3fbe35cee9b790fa646a3c676d95a16a39ecd
pxchecao <pepe1332016@gmail.com>
2025-08-19 11:28:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-function: qualquer
+
+const pageInput = document.getElementById("pageInput")
+const searchBtn = document.getElementById("searchBtn")
+const resultDiv = document.getElementById("results")
+
+async function fetchCharacters(page){
+    resultsDiv.innerHTML = "<p>Carregando..</p>"
+
+try {
+    const response = await fetch(`https://rickandmortyapi.com/api/character`)
+    const data = await response.json()
+    console.log(data)
+} catch (error) {
+    resultsDiv.innerHTML ="<p>Erro ao buscar personagens!</p>"
+    
+}
+}
+
+
+fetchCharacters(1)
+    
