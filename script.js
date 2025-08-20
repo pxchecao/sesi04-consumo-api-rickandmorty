fafff8b5456d64ace9166f907889a206ef3aabb5
pxchecao <pepe1332016@gmail.com>
2025-08-20 10:49:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,15 +7,42 @@ async function fetchCharacters(page){
     resultsDiv.innerHTML = "<p>Carregando..</p>"
 
 try {
-    const response = await fetch(`https://rickandmortyapi.com/api/character`)
+    const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
     const data = await response.json()
-    console.log(data)
-} catch (error) {
-    resultsDiv.innerHTML ="<p>Erro ao buscar personagens!</p>"
+//console.log(data)
+
+    if(data.error){
+        resultsDiv.innerHTML = "<p>Págino inválida!</p>"
+        return
+
+    }
+
+    resultsDiv.innerHTML = "";
+    data.results.forEach(character => { 
+        const card = document.createElement("div")
+        card.className = "card"
+        card.innerHTML = `
+            <img src="$(character.image}" alt="$(character.name}">
+            <h3>${character.name}</h3>
+            <p><strong>Status:</strong> ${character.status}</p>
+            <p><strong>Espécie:</strong> ${character.species}</p>
+        `
+        resultsDiv.appendChild(card)
+    });
     
-}
+} catch (error) {
+        resultsDiv.innerHTML ="<p>Erro ao buscar personagens!</p>"
+    
+    }
 }
 
-
+searchBtn.addEventListener("click", () => {
+    const page = pageInput.ariaValueMax.trim()
+    if(page){
+        fetchCharacters(page)
+    }else{
+        resultsDiv.innerHTML = "<p>Digite um número de página!</p>"
+    }
+})
 fetchCharacters(1)
     
