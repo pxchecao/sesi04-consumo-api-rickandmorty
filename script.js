3ca9cd26d56c4b47e1b288ca61324fdd2245430f
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-08-21 12:21:21 -0300

    wndewdene

--- script.js
+++ script.js
@@ -1,48 +1,46 @@
 
 const pageInput = document.getElementById("pageInput")
 const searchBtn = document.getElementById("searchBtn")
-const resultDiv = document.getElementById("results")
+const resultsDiv = document.getElementById("results")
 
 async function fetchCharacters(page){
     resultsDiv.innerHTML = "<p>Carregando..</p>"
 
-try {
-    const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
-    const data = await response.json()
-//console.log(data)
+    try {
+        const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
+        const data = await response.json()
+        // console.log(data)
 
-    if(data.error){
-        resultsDiv.innerHTML = "<p>Págino inválida!</p>"
-        return
+        if(data.error){
+            resultsDiv.innerHTML = "<p>Página inválida!</p>"
+            return
+        }
 
-    }
+        resultsDiv.innerHTML = "";
+        data.results.forEach(character => {
+            const card = document.createElement("div")
+            card.className = "card"
+            card.innerHTML = `
+                <img src="${character.image}" alt="${character.name}">
+                <h3>${character.name}</h3>
+                <p><strong>Status:</strong> ${character.status}</p>
+                <p><strong>Espécie:</strong> ${character.species}</p>
+            `
+            resultsDiv.appendChild(card)
+        });
 
-    resultsDiv.innerHTML = "";
-    data.results.forEach(character => { 
-        const card = document.createElement("div")
-        card.className = "card"
-        card.innerHTML = `
-            <img src="$(character.image}" alt="$(character.name}">
-            <h3>${character.name}</h3>
-            <p><strong>Status:</strong> ${character.status}</p>
-            <p><strong>Espécie:</strong> ${character.species}</p>
-        `
-        resultsDiv.appendChild(card)
-    });
-    
-} catch (error) {
-        resultsDiv.innerHTML ="<p>Erro ao buscar personagens!</p>"
-    
+    } catch (error) {
+          resultsDiv.innerHTML = "<p>Erro ao buscar personagens!</p>"
     }
 }
 
 searchBtn.addEventListener("click", () => {
-    const page = pageInput.ariaValueMax.trim()
+    const page = pageInput.value.trim()
     if(page){
         fetchCharacters(page)
     }else{
         resultsDiv.innerHTML = "<p>Digite um número de página!</p>"
     }
 })
+
 fetchCharacters(1)
-    
